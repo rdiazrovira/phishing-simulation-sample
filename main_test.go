225c4a1e3d11a724e4917848eed78f0832df41e7
Rodolfo Diaz <rdiaz@wawand.co>
2019-11-14 10:35:03 -0500

    test(main.go): add new unit tests

--- main_test.go
+++ main_test.go
@@ -3,35 +3,60 @@ package main
 import (
 	"bytes"
 	"io/ioutil"
-	"log"
 	"os"
 	"testing"
+
+	"github.com/PuerkitoBio/goquery"
 )
+
+var myHTML = `<body> <a href="https://myhtml.com">My HTML</a></body>`
 
 func TestCreateFileCopy(t *testing.T) {
 	filename := "wawandco.html"
 
-	file, err := os.Open(filename)
+	file, _ := os.Open(filename)
+
+	_, err := CreateFileCopy(file)
 	if err != nil {
-		log.Fatal(err)
+		t.Error(err)
 	}
 
-	_, err = CreateFileCopy(file)
+	source, err := ioutil.ReadFile("copy-wawandco.html")
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	f1, err := ioutil.ReadFile("wawandco.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	f2, err := ioutil.ReadFile("copy-wawandco.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if !bytes.Equal(f1, f2) {
 		t.Error("Not equals")
+	}
+
+	copy, err := ioutil.ReadFile("copy-wawandco.html")
+	if err != nil {
+		t.Error("Not equals")
+	}
+
+	if !bytes.Equal(source, copy) {
+		t.Error("Not equals")
+	}
+}
+
+func TestExtractHtmlFromFile(t *testing.T) {
+	_, err := ExtractHtmlFrom(myHTML)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestReplaceLinks(t *testing.T) {
+	doc, _ := ExtractHtmlFrom(myHTML)
+
+	ReplaceLinks(doc, "https://www.google.com")
+
+	count := 0
+	doc.Find("a").Each(func(i int, a *goquery.Selection) {
+		href, _ := a.Attr("href")
+		if href == "https://www.google.com" {
+			count++
+		}
+	})
+
+	if count != 1 {
+		t.Error("Links didn't change")
 	}
 }
